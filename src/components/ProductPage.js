e7f6301dcbc12930622188a8b91ead13860190ba
EPAFRA <enockepafra141@gmail..com>
2024-08-13 21:02:22 +0300

    proper display

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { removeProduct, addProduct, fetchProduct, updateProduct,fetchCategories } from '../api';
+import { removeProduct, addProduct, fetchProduct, updateProduct, fetchCategories } from '../api';
 
 function ProductPage() {
   const [products, setProducts] = useState([]);
@@ -55,7 +55,7 @@ function ProductPage() {
   };
 
   const handleEditProduct = async () => {
-    const updatedProduct =  await updateProduct(editProduct.id, editProduct);
+    const updatedProduct = await updateProduct(editProduct.id, editProduct);
     setProducts(products.map(product => product.id === updatedProduct.id ? updatedProduct : product));
     setEditProduct(null);
     setIsFormVisible(false); // Hide form after editing product
@@ -86,108 +86,112 @@ function ProductPage() {
       {/* <button onClick={toggleFormVisibility}>
         {isFormVisible ? "Cancel" : editProduct ? "Edit Product" : "Add Product"}
       </button> */}
-       <button onClick={handleAddButtonClick}>Add Product</button> {/* Button to add a new product */}
-      <table className="product-list">
-        <thead>
-          <tr>
-            {/* <th>ID</th> */}
-            <th>SKU</th>
-            <th>Product Name</th>
-            <th>Description</th>
-            <th>Manufacturer</th>
-            <th>Price</th>
-            <th>Out of stock</th>
-            <th>Category</th>
-            <th>quantity</th>
-            <th>Image</th>
-            {/* <th>Created At</th> */}
-            {/* <th>Updated At</th>  */}
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map(product => (
-            <tr key={product.id}>
-              {/* <td>{product.id}</td> */}
-              <td>{product.sku}</td>
-              <td>{product.name}</td>
-              <td>{product.description}</td>
-              <td>{product.manufacturer}</td>
-              <td>${product.price}</td>
-              <td>{product.is_out_of_stock}</td>
-              <td>{product.category_id}</td>
-              <td>{product.quantity}</td>
-              <td><img src={product.imageUrl} alt={product.name} style={{ width: '50px', height: '50px' }} /></td>
-              {/* <td>{product.createdAt}</td> */}
-              {/* <td>{product.updatedAt}</td> */}
-             
-              
-              <td>
-                {/* <button onClick={() => setEditProduct(product)}>Edit</button> */}
-                <button onClick={() => handleEditButtonClick(product)}>Edit</button>
-                <button onClick={() => handleDeleteProduct(product.id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      {!isFormVisible && (
+        <div>
+          <button onClick={handleAddButtonClick}>Add Product</button> {/* Button to add a new product */}
+          <table className="product-list">
+            <thead>
+              <tr>
+                {/* <th>ID</th> */}
+                <th>SKU</th>
+                <th>Product Name</th>
+                <th>Description</th>
+                <th>Manufacturer</th>
+                <th>Price</th>
+                <th>Out of stock</th>
+                <th>Category</th>
+                <th>quantity</th>
+                <th>Image</th>
+                {/* <th>Created At</th> */}
+                {/* <th>Updated At</th>  */}
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map(product => (
+                <tr key={product.id}>
+                  {/* <td>{product.id}</td> */}
+                  <td>{product.sku}</td>
+                  <td>{product.name}</td>
+                  <td>{product.description}</td>
+                  <td>{product.manufacturer}</td>
+                  <td>${product.price}</td>
+                  <td>{product.is_out_of_stock}</td>
+                  <td>{product.category_id}</td>
+                  <td>{product.quantity}</td>
+                  <td><img src={product.imageUrl} alt={product.name} style={{ width: '50px', height: '50px' }} /></td>
+                  {/* <td>{product.createdAt}</td> */}
+                  {/* <td>{product.updatedAt}</td> */}
+
+
+                  <td>
+                    {/* <button onClick={() => setEditProduct(product)}>Edit</button> */}
+                    <button onClick={() => handleEditButtonClick(product)}>Edit</button>
+                    <button onClick={() => handleDeleteProduct(product.id)}>Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
 
       {/* Form for adding or editing products */}
       {isFormVisible && (
-      <div className="product-form">
-        <h2>{editProduct ? "Edit Product" : "Add Product"}</h2>
-        {/* <input
+        <div className="product-form">
+          <h2>{editProduct ? "Edit Product" : "Add Product"}</h2>
+          {/* <input
           type="text"
           placeholder="SKU"
           value={editProduct ? editProduct.sku : newProduct.sku}
           onChange={(e) => editProduct ? setEditProduct({ ...editProduct, sku: e.target.value }) : setNewProduct({ ...newProduct, sku: e.target.value })}
         /> */}
-        <input
-          type="text"
-          placeholder="Product Name"
-          value={editProduct ? editProduct.name : newProduct.name}
-          onChange={(e) => editProduct ? setEditProduct({ ...editProduct, name: e.target.value }) : setNewProduct({ ...newProduct, name: e.target.value })}
-        />
-        <input
-          type="text"
-          placeholder="Description"
-          value={editProduct ? editProduct.description : newProduct.description}
-          onChange={(e) => editProduct ? setEditProduct({ ...editProduct, description: e.target.value }) : setNewProduct({ ...newProduct, description: e.target.value })}
-        />
-        <input
-          type="text"
-          placeholder="Manufacturer"
-          value={editProduct ? editProduct.manufacturer : newProduct.manufacturer}
-          onChange={(e) => editProduct ? setEditProduct({ ...editProduct, manufacturer: e.target.value }) : setNewProduct({ ...newProduct, manufacturer: e.target.value })}
-        />
-        <input
-          type="number"
-          placeholder="Price"
-          value={editProduct ? editProduct.price : newProduct.price}
-          onChange={(e) => editProduct ? setEditProduct({ ...editProduct, price: e.target.value }) : setNewProduct({ ...newProduct, price: e.target.value })}
-        />
-       <select
-          value={editProduct ? editProduct.category_id : newProduct.category_id}
-          onChange={(e) => editProduct ? setEditProduct({ ...editProduct, category_id: e.target.value }) : setNewProduct({ ...newProduct, category_id: e.target.value })}
-        >
-          <option value="">Select Category</option>
-          {categories.map(cat => (
-            <option key={cat.id} value={cat.id}>{cat.name}</option>
-          ))}
-        </select>
-        <input
-          type="number"
-          placeholder="Quantity"
-          value={editProduct ? editProduct.quantity : newProduct.quantity}
-          onChange={(e) => editProduct ? setEditProduct({ ...editProduct, quantity: e.target.value }) : setNewProduct({ ...newProduct, quantity: e.target.value })}
-        />
-        <input
-          type="text"
-          placeholder="Image URL"
-          value={editProduct ? editProduct.imageUrl : newProduct.imageUrl}
-          onChange={(e) => editProduct ? setEditProduct({ ...editProduct, imageUrl: e.target.value }) : setNewProduct({ ...newProduct, imageUrl: e.target.value })}
-        />
-        {/* <label>
+          <input
+            type="text"
+            placeholder="Product Name"
+            value={editProduct ? editProduct.name : newProduct.name}
+            onChange={(e) => editProduct ? setEditProduct({ ...editProduct, name: e.target.value }) : setNewProduct({ ...newProduct, name: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Description"
+            value={editProduct ? editProduct.description : newProduct.description}
+            onChange={(e) => editProduct ? setEditProduct({ ...editProduct, description: e.target.value }) : setNewProduct({ ...newProduct, description: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Manufacturer"
+            value={editProduct ? editProduct.manufacturer : newProduct.manufacturer}
+            onChange={(e) => editProduct ? setEditProduct({ ...editProduct, manufacturer: e.target.value }) : setNewProduct({ ...newProduct, manufacturer: e.target.value })}
+          />
+          <input
+            type="number"
+            placeholder="Price"
+            value={editProduct ? editProduct.price : newProduct.price}
+            onChange={(e) => editProduct ? setEditProduct({ ...editProduct, price: e.target.value }) : setNewProduct({ ...newProduct, price: e.target.value })}
+          />
+          <select
+            value={editProduct ? editProduct.category_id : newProduct.category_id}
+            onChange={(e) => editProduct ? setEditProduct({ ...editProduct, category_id: e.target.value }) : setNewProduct({ ...newProduct, category_id: e.target.value })}
+          >
+            <option value="">Select Category</option>
+            {categories.map(cat => (
+              <option key={cat.id} value={cat.id}>{cat.name}</option>
+            ))}
+          </select>
+          <input
+            type="number"
+            placeholder="Quantity"
+            value={editProduct ? editProduct.quantity : newProduct.quantity}
+            onChange={(e) => editProduct ? setEditProduct({ ...editProduct, quantity: e.target.value }) : setNewProduct({ ...newProduct, quantity: e.target.value })}
+          />
+          <input
+            type="text"
+            placeholder="Image URL"
+            value={editProduct ? editProduct.imageUrl : newProduct.imageUrl}
+            onChange={(e) => editProduct ? setEditProduct({ ...editProduct, imageUrl: e.target.value }) : setNewProduct({ ...newProduct, imageUrl: e.target.value })}
+          />
+          {/* <label>
           Out of Stock:
           <input
             type="checkbox"
@@ -195,10 +199,10 @@ function ProductPage() {
             onChange={(e) => editProduct ? setEditProduct({ ...editProduct, is_out_of_stock: e.target.checked }) : setNewProduct({ ...newProduct, is_out_of_stock: e.target.checked })}
           />
         </label> */}
-        <button onClick={editProduct ? handleEditProduct : handleAddProduct}>
-          {editProduct ? "Update Product" : "Add Product"}
-        </button>
-      </div>
+          <button onClick={editProduct ? handleEditProduct : handleAddProduct}>
+            {editProduct ? "Update Product" : "Add Product"}
+          </button>
+        </div>
       )}
     </div>
   );
